9598f5e81dc6fb4eca7b5d416f6c9c71bbd2a087
user2 <user2@user2-PC>
2019-08-29 20:29:31 +0900

    add print message 안녕

--- gittest/src/com/git/main/Main.java
+++ gittest/src/com/git/main/Main.java
@@ -3,7 +3,9 @@ package com.git.main;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("내가 만든것");
+		
+		System.out.println("여러분 깃 재밌죠?");
 
 	}
 
