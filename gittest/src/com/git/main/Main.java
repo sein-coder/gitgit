d1831f3c8894aa283ee595dd1be3bb2a5527db8a
user2 <user2@user2-PC>
2019-08-29 20:45:56 +0900

    Revert "add print message 안녕"
    This reverts commit 9598f5e81dc6fb4eca7b5d416f6c9c71bbd2a087.

--- gittest/src/com/git/main/Main.java
+++ gittest/src/com/git/main/Main.java
@@ -3,9 +3,7 @@ package com.git.main;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("내가 만든것");
-		
-		System.out.println("여러분 깃 재밌죠?");
+		// TODO Auto-generated method stub
 
 	}
 
